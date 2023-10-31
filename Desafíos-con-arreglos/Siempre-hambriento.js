77881505dcab6bdd2464417a26652cfbc3bbaa89
daniel234423 <dnulapi@gmail.com>
2023-10-31 11:30:37 -0300

    calculadora

---
+++ Desafíos-con-arreglos/Siempre-hambriento.js
@@ -0,0 +1,16 @@
+function siempreHambriento(arr) {
+    let found = false;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "comida") {
+            console.log(arr[i] + " delicioso");
+            found = true;
+        }
+    }
+
+    if (!found) {
+        console.log("Tengo hambre");
+    }
+}
+siempreHambriento(["comida", "agua", "suelo"]); // Debería mostrar "delicioso" en la consola.
+siempreHambriento([4, 1, 5, 7, 2]); // Debería mostrar "Tengo hambre" en la consola.
