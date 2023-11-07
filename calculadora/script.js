44853f28ead8e5db3386c1b941045752cbf10080
daniel234423 <dnulapi@gmail.com>
2023-11-07 08:39:34 -0300

    xd

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,5 +1,19 @@
+var display = document.querySelector('#display');
 function press(numbers) {
-    var display = document.querySelector('#display');
-    display.innerHTML += numbers;
+    if (display.textContent === "0") {
+    display.textContent = numbers;
+    }else{
+        display.textContent += numbers;
+    }
+}
 
+function clr() {
+    display.innerHTML = '0';
+}
+function setOP(Operators) {
+    display.textContent += Operators;
+    
+}
+function calculate() {
+    display.textContent = eval(display.textContent);
 }
